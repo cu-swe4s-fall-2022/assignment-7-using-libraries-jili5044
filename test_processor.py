a9f701830d56339510cd6636560e21ccf32c81b3
jili5044 <jili5044@colorado.edu>
2022-10-23 06:15:32 +0000

    fix style

--- test_processor.py
+++ test_processor.py
@@ -9,14 +9,13 @@ class TestUtils(unittest.TestCase):
         num_rows = 3
         num_columns = 3
         self.assertEqual(dp.get_random_matrix(num_rows, num_columns).shape,
-                         (3,3))
+                         (3, 3))
 
     def test_get_file_dimensions(self):
         file_name = 'iris.data'
-        self.assertEqual(dp.get_file_dimensions(file_name), (150,5))
-        self.assertNotEqual(dp.get_file_dimensions(file_name), (150,4))
+        self.assertEqual(dp.get_file_dimensions(file_name), (150, 5))
+        self.assertNotEqual(dp.get_file_dimensions(file_name), (150, 4))
 
 
 if __name__ == '__main__':
     unittest.main()
-
